0d6ce0d01989a45873350a4d02338697daa07de9
naman <sushillodhi671@gmail.com>
2024-08-27 16:25:47 +0530

    new logic for cookies

--- cookies/myapp/views.py
+++ cookies/myapp/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 def index(request):
-    # Retrieve theme from cookies 
     theme = request.COOKIES.get('theme', 'light')
     return render(request, 'base.html', {'theme': theme})
 
 def set_theme(request, theme):
-    response = redirect('index')  # Redirect to the main page
     if theme in ['light', 'dark']:
+        response = redirect('index')
         response.set_cookie('theme', theme, max_age=60*60*24*30)  # Cookie expires in 30 days
-    return response
+        return response
+    return redirect('index')
