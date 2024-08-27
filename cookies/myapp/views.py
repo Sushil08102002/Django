23748a85dd46c523d45d051bbecc5548260d833c
naman <namanshrivastava40@gmail.com>
2024-08-27 12:30:29 +0530

    new task of cookies

---
+++ cookies/myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+
+def index(request):
+    # Retrieve theme from cookies or default to 'light'
+    theme = request.COOKIES.get('theme', 'light')
+    return render(request, 'base.html', {'theme': theme})
+
+def set_theme(request, theme):
+    response = redirect('index')  # Redirect to the main page
+    if theme in ['light', 'dark']:
+        response.set_cookie('theme', theme, max_age=60*60*24*30)  # Cookie expires in 30 days
+    return response
